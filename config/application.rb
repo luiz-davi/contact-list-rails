220e4981d2bcb0d0531938b3a30c4c0af57ddbbc
Luiz Davi <55983920+luiz-davi@users.noreply.github.com>
2021-12-29 21:38:48 -0300

    Internacionalizando a aplicação

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module ContactList
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # tradução
+    config.i18n.default_locale = :'pt-BR'
   end
 end
