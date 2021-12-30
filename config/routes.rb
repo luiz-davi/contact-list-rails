57bea21e378f285e049232ef407094044397835f
Luiz Davi <55983920+luiz-davi@users.noreply.github.com>
2021-12-29 21:23:36 -0300

    Formulario para criação de usuário e validates

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  resources :contacts
+  
   root to: 'static_pages#index'
 
   get 'sobre' => 'static_pages#sobre'
   get 'contato' => 'static_pages#contato'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # resources
+  resources :contacts
+  resources :users, only: [:new, :create]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -23,9 +23,9 @@ ActiveRecord::Schema.define(version: 2021_12_29_181457) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "name"
-    t.string "password_digest"
+    t.string "email", default: "", null: false
+    t.string "name", default: "", null: false
+    t.string "password_digest", default: "", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
